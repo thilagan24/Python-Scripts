1fcefe905b73e8399beba992b6cb4724a397f89a
thilagan240491 <thilagan24@gmail.com>
2026-03-31 18:52:58 +0530

    adding program3

---
+++ Program3_SimpleSwapWOVariabe.py
@@ -0,0 +1,11 @@
+''' This is a Simple program.
+
+Swapping value of 2 variable without using a 3rd temporary variable '''
+
+x,y=4,5 #this is how you assign values to multiple variables at once.
+
+print("Values before swapping",x,y)
+
+x,y=y,x #Same multi assigning value for variables is used here
+
+print("Values After swapping",x,y)
